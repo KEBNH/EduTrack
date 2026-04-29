2ce15138a33ed68af2a47247c25ffb7639e5e957
BOTk <teampoison64@gmail.com>
2026-04-28 19:33:03 -0500

    Cambios en el CD/CI - Proceso de deploy con Render

--- EduTrackApp/urls.py
+++ EduTrackApp/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path
 
+def health_check(request):
+    return JsonResponse({"status": "ok"}, status=200)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health',health_check)
 ]
