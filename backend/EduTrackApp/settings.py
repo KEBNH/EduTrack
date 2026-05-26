f3e37517cf8b7e16e558fea3b05b178297a7b42b
BOTk <teampoison64@gmail.com>
2026-05-25 20:13:46 -0500

    LandPage agregada

--- backend/EduTrackApp/settings.py
+++ backend/EduTrackApp/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = BASE_DIR.parent
+FRONTEND_DIR = PROJECT_ROOT / 'frontend'
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            FRONTEND_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [FRONTEND_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
