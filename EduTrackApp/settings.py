5ebe4a3dd6ce90c13fbd4acec5668e478e0d2497
BOTk <teampoison64@gmail.com>
2026-05-26 21:27:02 -0500

    fix proyecto

--- EduTrackApp/settings.py
+++ EduTrackApp/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'EduTrack.urls'
+ROOT_URLCONF = 'EduTrackApp.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'EduTrack.wsgi.application'
+WSGI_APPLICATION = 'EduTrackApp.wsgi.application'
 
 
 # Database
