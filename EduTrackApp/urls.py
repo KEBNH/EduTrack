0954d81ccfe0656585f4b036d5c4e123649f389d
BOTk <teampoison64@gmail.com>
2026-07-05 11:44:46 -0500

    Agrega landing page publica

--- EduTrackApp/urls.py
+++ EduTrackApp/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from academico.views import inicio
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', inicio, name='inicio'),
+    path('', TemplateView.as_view(template_name='public/landing.html'), name='landing'),
+    path('dashboard/', inicio, name='inicio'),
     path('academico/', include('academico.urls')),
 ]
